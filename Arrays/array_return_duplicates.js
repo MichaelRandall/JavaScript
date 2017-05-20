d60a15a20b6e394804b766c76c9203cb3919b462
Mike <m_ran66@hotmail.com>
2017-05-20 14:17:01 -0700

    create returnDuplicates function to take an array and return only duplicate values

---
+++ Arrays/array_return_duplicates.js
@@ -0,0 +1,16 @@
+function returnDuplicates(arr){
+  var i,
+  arrLength = arr.length,
+  obj = {},
+  results = [];
+
+
+  for (i=0; i < arrLength; i++){
+    if(typeof(obj[arr[i]]) === "undefined"){
+      obj[arr[i]] = 0;
+    }else{
+      results.push(arr[i]);
+    }
+  }
+  return results;
+}
