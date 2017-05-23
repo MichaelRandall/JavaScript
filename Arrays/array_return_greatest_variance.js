836c4a7da2d23840658a2e5543b62e9e4d527da5
m_ran66 <m_ran66@hotmail.com>
2017-05-22 18:16:53 -0700

    add capability to determine greatest variance in array values

---
+++ Arrays/array_return_greatest_variance.js
@@ -0,0 +1,36 @@
+function getMaxVariance(arr){
+  var i,j,
+  arrLen = arr.length,
+  elemHldArr = [],
+  valHldrArr = [],
+  bigVar = 0;
+
+  for(i = 0; i < arrLen; i++){
+    //console.log("Begin loop array-i = " + arr[i]);
+    for(j = i; j < arrLen; j++){
+    //console.log("Begin loop array-j = " + arr[j]);
+      if(arr[i] < arr[j]){
+        elemHldArr.push([arr[i],arr[j]]);
+        valHldrArr.push(arr[j] - arr[i]);
+      }
+      // if(arr[i] < arr[j]){
+      //   console.log("arrayi is less than arrayj - subtrack i from j");
+      //   var ai = arr[i];
+      //   var aj = arr[j];
+      //   console.log("arrayi = " + arr[i] + ", arrayj = " + arr[j] + ". The difference is: " + (arr[j] - arr[i]));
+      //   hldArr.push(aj - ai);
+      //   //console.log("Ai: " + arr[i] + " Aj: " + arr[j]);
+      //   //bigVar = arr[j] - arr[i];
+      // }else{
+      //   console.log("arrayi is greater than or equal to arrayj - do nothing with it");
+      // }
+    }
+  }
+  for(var k = 0; k < elemHldArr.length; k++){
+    console.log(elemHldArr[k].toString() + "\n");
+  }
+  console.log(valHldrArr);
+  console.log(arr);
+  var biggie = Math.max.apply(null,valHldrArr);
+  console.log(biggie);
+}
